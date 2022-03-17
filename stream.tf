9fdd73d24a60778b6dae11b9aef3ce26aea6b037
ChristoPedro <pedroegcj@hotmail.com>
2022-03-17 17:22:03 -0300

    Update Stream

--- stream.tf
+++ stream.tf
@@ -7,6 +7,6 @@ resource "oci_streaming_stream_pool" "stream_pool" {
 resource "oci_streaming_stream" "stream" {
     #Required
     name = var.stream_name
-    partitions = var.stream_partitionsgit
+    partitions = var.stream_partitions
     stream_pool_id = oci_streaming_stream_pool.stream_pool.id
 }
